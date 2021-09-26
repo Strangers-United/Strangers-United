97c5617ece007fc7267416c23b36cb830096a3fb
cklung5 <kit.cklung@gmail.com>
2021-09-25 22:36:25 -0400

    able to push data to ipfs

--- client/src/containers/IPFS/index.tsx
+++ client/src/containers/IPFS/index.tsx
@@ -1,5 +1,36 @@
+import { create as ipfsCreate } from "ipfs-http-client";
+import { useState } from "react";
+
 const Ipfs = () => {
-    return <div>Ipfs page</div>;
+    const [text, setText] = useState("");
+    const [url, setUrl] = useState("");
+
+    const submit = async () => {
+        try {
+            const client = ipfsCreate({
+                host: "ipfs.infura.io",
+                port: 5001,
+                protocol: "https",
+            });
+            const { cid } = await client.add(text);
+            setUrl(`https://ipfs.io/ipfs/${cid.toV1()}`);
+        } catch (err) {
+            console.log("==== err", err);
+        }
+    };
+
+    return (
+        <div>
+            <div>Your text {text}</div>
+            <div>url : {url}</div>
+            <input
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setText(e.target.value)
+                }
+            />
+            <button onClick={submit}>submit</button>
+        </div>
+    );
 };
 
 export default Ipfs;
