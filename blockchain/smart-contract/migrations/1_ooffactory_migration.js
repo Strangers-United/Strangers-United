ab38f7434e9aff08ed78787a97e1bef2b5c7a383
Mohammadreza <hajbabaei.mr@gmail.com>
2021-09-27 09:08:17 +0330

    remove contracts related to Conjure (temporarily)

--- blockchain/smart-contract/migrations/1_ooffactory_migration.js
+++ blockchain/smart-contract/migrations/1_ooffactory_migration.js
@@ -2,8 +2,8 @@ const oofFactory = artifacts.require("OOFFactory");
 
 module.exports = async function (deployer, network, accounts) {
   // deployment steps
-  factoryOwner = "";
-  oofImplementation = "";
-  conjureRouter = "";
-  await deployer.deploy(oofFactory, );
+  const factoryOwner = "";
+  const oofImplementation = "";
+  const conjureRouter = "";
+  await deployer.deploy(oofFactory, factoryOwner, oofImplementation, conjureRouter);
 };
